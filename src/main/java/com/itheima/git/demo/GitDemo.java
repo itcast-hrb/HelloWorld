c808ba56fa2c48a6645b8ce7ed6383bde7d95f90
itcast-hrb <72645339+itcast-hrb@users.noreply.github.com>
2020-10-10 19:08:10 +0800

    Update GitDemo.java

--- src/main/java/com/itheima/git/demo/GitDemo.java
+++ src/main/java/com/itheima/git/demo/GitDemo.java
@@ -3,5 +3,6 @@ package com.itheima.git.demo;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Git!");
     }
 }
